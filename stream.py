1710b76c170d59b48620242996e65713516e55af
Velik Kicukov <vkicukov@verticaldynamix.com>
2024-02-15 15:00:13 +0100

    Initial commit33

--- stream.py
+++ stream.py
@@ -9,6 +9,15 @@ username = 'VerticalDynamix'
 password = 'Golemw#153'
 driver = '{ODBC Driver 17 for SQL Server}'
 
+def marketAll(conn):
+    results=[]
+    cursor=conn.cursor()
+    cursor.execute(
+        """select MarketName from MarketID order by MarketName ASC"""
+    )
+    for row in cursor:
+        results.append(list(row)[0])
+    return results
 
 def readProgressTracker(Market):
     connection_string = f'mssql+pyodbc://{username}:{password}@{server}/{database}?driver=ODBC+Driver+17+for+SQL+Server'
@@ -25,25 +34,40 @@ def readProgressTracker(Market):
 
 def main():
     # Title
-    st.title("Simple Dashboard ddddd")
+    st.title("Progress Tracker")
 
-    # Load data
-    data = load_data()
+        # Establish database connection
+    
+    connection_string = f"Driver={driver};Server={server};Database={database};Uid={username};Pwd={password};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
+    conn = pyodbc.connect(connection_string)
+
+    # connection_string = 'mssql+pyodbc://username:password@server/database?driver=ODBC+Driver+17+for+SQL+Server'
+    # engine = create_engine(connection_string)
+
+    options = marketAll(conn)
+    selected_option = st.selectbox('Select Market', options)
+
+    # st.write('Selected Options:', selected_option)
+     # Load data for the selected option
+    data = readProgressTracker(selected_option)
 
     # Display data table
-    st.write("## Data Table")
-    st.write(data)
+    if not data.empty:
+        st.write("## Data Table")
+        st.write(data)
+    else:
+        st.write("No data available for the selected option.")
 
-def load_data():
-    # Sample data
-    # data = {
-    #     'Name': ['John', 'Alice', 'Bob', 'Jane'],
-    #     'Age': [25, 30, 35, 40],
-    #     'Location': ['New York', 'Los Angeles', 'Chicago', 'Houston']
-    # }
-    data=readProgressTracker('Duluth')
-    df = pd.DataFrame(data)
-    return df
+# def load_data():
+#     # Sample data
+#     # data = {
+#     #     'Name': ['John', 'Alice', 'Bob', 'Jane'],
+#     #     'Age': [25, 30, 35, 40],
+#     #     'Location': ['New York', 'Los Angeles', 'Chicago', 'Houston']
+#     # }
+#     data=readProgressTracker('Duluth')
+#     df = pd.DataFrame(data)
+#     return df
 
 if __name__ == "__main__":
     main()
